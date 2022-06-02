39aa73cc6870c7c14a0591c00cb5abe934579a4c
Dalton Eggleston <egglestondalton@yahoo.com>
2022-06-02 06:54:42 -0700

    still confused

--- python/caesar_cipher.py
+++ python/caesar_cipher.py
@@ -1,12 +1,12 @@
+import re
 def caesar_cipher(string, shift_amount):
-
     standard_alphabet = "abcdefghijklmnopqrstuvwxyz"
     capital_alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     original_letter = ""
     shifted_letter = ""
     word_to_list = []
     string_to_number = int(shift_amount)
-
+    
     for x in range(0, len(string)):
         if re.match("[^a-zA-Z]", string[x]):
             word_to_list.append(string[x])
@@ -24,7 +24,7 @@ def caesar_cipher(string, shift_amount):
         shifted_letter = standard_alphabet[new_number_calculator]
         if standard_alphabet.find(string[x]) == -1:
             shifted_letter = capital_alphabet[new_number_calculator]
-        word_to_list.append(string[x])
-    finalResult = word_to_list.join("")
+        word_to_list.append(shifted_letter)
+        finalResult = "".join(word_to_list)
     print(finalResult)
     return finalResult
