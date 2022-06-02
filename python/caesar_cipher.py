ab84ca877de624b7d4205a1a00d4d5481868b004
Dalton Eggleston <egglestondalton@yahoo.com>
2022-06-01 21:42:30 -0700

    regex error

--- python/caesar_cipher.py
+++ python/caesar_cipher.py
@@ -1,1 +1,30 @@
 def caesar_cipher(string, shift_amount):
+
+    standard_alphabet = "abcdefghijklmnopqrstuvwxyz"
+    capital_alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    original_letter = ""
+    shifted_letter = ""
+    word_to_list = []
+    string_to_number = int(shift_amount)
+
+    for x in range(0, len(string)):
+        if re.match("[^a-zA-Z]", string[x]):
+            word_to_list.append(string[x])
+            continue
+        original_letter = standard_alphabet.find(string[x])
+        if original_letter == -1:
+            original_letter = capital_alphabet.find(string[x])
+        
+        new_number_calculator = original_letter + string_to_number
+        if new_number_calculator >= 26:
+            new_number_calculator -= 28
+        if new_number_calculator < 0:
+            new_number_calculator += 26
+        
+        shifted_letter = standard_alphabet[new_number_calculator]
+        if standard_alphabet.find(string[x]) == -1:
+            shifted_letter = capital_alphabet[new_number_calculator]
+        word_to_list.append(string[x])
+    finalResult = word_to_list.join("")
+    print(finalResult)
+    return finalResult
